8603b7ac7b61d00b0a88dfabe12b28a14ad92dd1
zericktao <zericktao@qq.com>
2026-07-26 00:05:20 +0800

    feat(w1d5): qwen3 embedding

--- src/tiny_llm/embedding.py
+++ src/tiny_llm/embedding.py
@@ -4,13 +4,23 @@ from .quantize import QuantizedWeights
 
 class Embedding:
     def __init__(self, vocab_size: int, embedding_dim: int, weight: mx.array):
-        pass
+        self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
+        self.weight = weight
 
     def __call__(self, x: mx.array) -> mx.array:
-        pass
-
+        return self.weight[x]
+    
+    # Embedding::as_linear
+    # weight: vocab_size x embedding_dim
+    # Input: N.. x embedding_dim
+    # Output: N.. x vocab_size
     def as_linear(self, x: mx.array) -> mx.array:
-        pass
+        # 将嵌入层视为线性层
+        # weight: (vocab_size, embedding_dim)
+        # x: (batch_size, seq_len)
+        # output: (batch_size, seq_len, embedding_dim)
+        return x @ self.weight.T
 
 
 class QuantizedEmbedding:
